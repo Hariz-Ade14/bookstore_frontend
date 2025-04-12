2f0cd4e65e499bdf26d7d3934da92a947a3b36b9
Harizz_TheGreat <harizadebayo8@gmail.com>
2025-04-12 03:49:27 -0700

    Update protectedRoutes.tsx

--- src/components/protectedRoutes.tsx
+++ src/components/protectedRoutes.tsx
@@ -1,14 +1,10 @@
-import React,{useEffect} from "react";
+import React from "react";
 import useAuthStore from "../store/useAuthStore";
 import { Navigate } from "react-router-dom";
 const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
   const { token } = useAuthStore();
   if(!token){
     return <Navigate to="/" />; 
-  }
-
-  if(token) {
-    return <>{children} </>
   }
   return <>{children}</> 
 };
